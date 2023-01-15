995f617249b62d7b41bb5d1f60044e05dbdb9cc4
rtBot <43742164+rtBot@users.noreply.github.com>
2023-01-16 04:44:08 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:@wordpress/eslint-plugin/recommended',
+		'plugin:eslint-comments/recommended',
+	],
+	env: {
+		browser: true,
+	},
+	rules: {
+		// Add rules.
+	},
+};
