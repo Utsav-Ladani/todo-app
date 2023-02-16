8f992146c4130226aa5acb21879f6e34f02d70f9
Utsav-Ladani <201901076@daiict.ac.in>
2023-02-16 15:50:11 +0530

    Add constants to make config easy.

---
+++ plugins/movie-library/plugin-constant.php
@@ -0,0 +1,12 @@
+<?php
+
+// define constants below.
+/**
+ * @const string MOVIE_LIBRARY_PLUGIN_DIR Plugin directory path.
+ */
+define( "MOVIE_LIBRARY_PLUGIN_DIR", plugin_dir_path(__FILE__) );
+
+/**
+ * @const string MOVIE_LIBRARY_NAMESPACE Plugin namespace.
+ */
+define( "MOVIE_LIBRARY_NAMESPACE", __NAMESPACE__ );
