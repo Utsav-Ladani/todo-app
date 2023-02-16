b37f9ad244115aba6533cd133234335a5cf30e6f
Utsav-Ladani <201901076@daiict.ac.in>
2023-02-16 10:58:01 +0530

    Add the plugin header.

---
+++ plugins/movie-library/movie-library.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Movie Library
+ *
+ * @package           MovieLibrary
+ * @author            Utsav Ladani
+ * @copyright         2023 by Utsav Ladani
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Movie Library
+ * Plugin URI:        https://example.com/movie-library
+ * Description:       Plugin to manage movie library
+ * Version:           1.0.0
+ * Requires at least: 6.1
+ * Requires PHP:      8.2
+ * Author:            Utsav Ladani
+ * Author URI:        https://github.com/Utsav-Ladani/
+ * Text Domain:       movie-library
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */
