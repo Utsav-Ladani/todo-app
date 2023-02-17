4240854be8450d20bbc2071c68c382c786fb70a8
Utsav-Ladani <201901076@daiict.ac.in>
2023-02-17 11:41:23 +0530

    Add constants for version and plugin url.

--- plugins/movie-library/plugin-constant.php
+++ plugins/movie-library/plugin-constant.php
@@ -12,6 +12,11 @@ namespace Movie_Library;
 
 // define constants below.
 /**
+ * @const string MOVIE_LIBRARY_VERSION Plugin version.
+ */
+define('MOVIE_LIBRARY_VERSION', '1.0.0');
+
+/**
  * @const string MOVIE_LIBRARY_PLUGIN_DIR Plugin directory path.
  */
 define( "MOVIE_LIBRARY_PLUGIN_DIR", plugin_dir_path(__FILE__) );
@@ -20,3 +25,8 @@ define( "MOVIE_LIBRARY_PLUGIN_DIR", plugin_dir_path(__FILE__) );
  * @const string MOVIE_LIBRARY_NAMESPACE Plugin namespace.
  */
 define( "MOVIE_LIBRARY_NAMESPACE", __NAMESPACE__ );
+
+/**
+ * @const string MOVIE_LIBRARY_PLUGIN_URL Plugin directory URL.
+ */
+define( "MOVIE_LIBRARY_PLUGIN_URL", plugin_dir_url(__FILE__) );
