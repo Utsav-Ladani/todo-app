012f279ec56ae71e940530e40e3aa61460279a88
Utsav-Ladani <201901076@daiict.ac.in>
2023-02-21 01:30:21 +0530

    Write secure uninstall.php file

---
+++ plugins/movie-library/uninstall.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Movie Library uninstall
+ * Uninstalling Movie Library deletes user roles, pages, tables, and options according to the settings.
+ *
+ * @package MovieLibrary
+ * @author  Utsav Ladani
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+die("Uninstalling plugin...");
