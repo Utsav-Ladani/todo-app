c3299d3bd3ba3f70aeeefe36fc7cb174ea1fb4e7
Utsav-Ladani <201901076@daiict.ac.in>
2023-02-22 16:13:46 +0530

    Add image meta box to movie and person post type.

---
+++ plugins/movie-library/admin/js/image-upload-handler.js
@@ -0,0 +1,151 @@
+/**
+ * Image Upload Handler
+ *
+ * @version 1.0.0
+ */
+
+// call the function with jQuery
+jQuery(function ($) {
+	// run it after the DOM is ready
+	$(document).ready(function ($) {
+		// get i18n functions
+		const { __ } = wp.i18n;
+
+		// get DOM objects
+		const metaBox = $('#movie-library-image-upload-handler');
+		const addImageButton = metaBox.find('#add-images-custom-btn');
+		const imagePreviewContainer = metaBox.find('#image-preview-container');
+		const imageInput = metaBox.find('#rt-upload-images');
+		const removeImageButton = metaBox.find('.rt-remove-image-btn');
+
+		// init frame object
+		let frame = null;
+
+		// remove image action handler
+		const removeImageHandler = function (eventRemove) {
+			eventRemove.preventDefault();
+
+			// get image id
+			let id = $(this).attr('data-image-id');
+			id = parseInt(id);
+
+			// remove image from value
+			let value = imageInput.val();
+			value = value.split(',');
+			value = value.filter(function (item) {
+				return item !== String(id);
+			});
+
+			// update input value
+			value = value.join(',');
+			imageInput.val(value);
+
+			// remove image preview box
+			$(this).parent().remove();
+		};
+
+		// add listener on remove image button
+		removeImageButton.on('click', removeImageHandler);
+
+		// add listener on add image button
+		addImageButton.on('click', function (event) {
+			event.preventDefault();
+
+			// open media frame if already exists
+			if (frame) {
+				frame.open();
+				return;
+			}
+
+			// create media frame
+			frame = wp.media({
+				title: __('Select or Upload Images for Movie', 'movie-library'),
+				button: {
+					text: __('Add Images', 'movie-library'),
+				},
+				library: {
+					type: 'image',
+				},
+				multiple: true,
+			});
+
+			// add listener on select images
+			frame.on('select', function () {
+				// get selected images
+				const images = frame.state().get('selection').toJSON();
+
+				// get value from input
+				let rawValue = imageInput.val().split(',');
+
+				// remove empty values
+				rawValue = rawValue.filter(function (item) {
+					return item !== '';
+				});
+
+				const imageIDs = rawValue;
+				const imageTags = [];
+
+				// add images to preview box
+				images.forEach(function (image) {
+					// validate the URL
+					let imageURL = null;
+					try {
+						imageURL = new URL(image.url);
+					} catch (e) {
+						// console.error('Wrong URL');
+						return;
+					}
+
+					// check if image already exists
+					if (imageIDs.includes(String(image.id))) {
+						return;
+					}
+
+					// create image tag
+					const img = $('<img />', {
+						src: imageURL,
+						class: 'widefat',
+					});
+
+					// create remove button
+					const btn = $('<button />', {
+						class: 'button rt-remove-image-btn widefat',
+						'data-image-id': image.id,
+						text: __('Remove', 'movie-library'),
+					});
+
+					// add listener on remove button
+					btn.on('click', removeImageHandler);
+
+					// create image container
+					const div = $('<div />', {
+						class: 'image-item',
+						'data-image-id': image.id,
+					});
+
+					// append image and button to container
+					div.append(img);
+					div.append(btn);
+
+					// add container to array
+					imageTags.push(div);
+
+					// add image id to array if it is valid
+					if (parseInt(image.id)) {
+						imageIDs.push(image.id);
+					}
+				});
+
+				// update input value
+				const value = imageIDs.join(',');
+				imageInput.val(value);
+
+				// append images to preview box
+				imagePreviewContainer.append(imageTags);
+			});
+
+			// open media frame
+			frame.open();
+		});
+	});
+});
---
+++ plugins/movie-library/meta-box/class-images-meta-box.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ * Images Meta Box.
+ * This class is responsible for adding image meta box to movie and person post type.
+ *
+ * @package Movie_Library
+ */
+
+namespace Movie_Library\Meta_Box;
+
+/**
+ * Class Images_Meta_Box
+ * Adds images meta box to movie and person post type.
+ */
+class Images_Meta_Box {
+	/**
+	 * Initialize the class.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 * @hooked action add_meta_boxes
+	 * @hooked action admin_enqueue_scripts
+	 * @hooked action save_post_rt-movie
+	 * @hooked action save_post_rt-person
+	 */
+	public static function init() : void {
+		add_action( 'add_meta_boxes', array( __CLASS__, 'add_image_meta_box' ) );
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'add_image_media_script' ) );
+		add_action( 'save_post_rt-movie', array( __CLASS__, 'save_post_images' ) );
+		add_action( 'save_post_rt-person', array( __CLASS__, 'save_post_images' ) );
+	}
+
+	/**
+	 * Adds images meta box to movie and person post type.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 */
+	public static function add_image_meta_box() : void {
+		// Add images meta box to movie post type.
+		add_meta_box(
+			'rt-media-meta-images',
+			__( 'Images', 'movie-library' ),
+			array( __CLASS__, 'render_image_meta_box' ),
+			'rt-movie',
+			'side',
+		);
+
+		// Add images meta box to person post type.
+		add_meta_box(
+			'rt-media-meta-images',
+			__( 'Images', 'movie-library' ),
+			array( __CLASS__, 'render_image_meta_box' ),
+			'rt-person',
+			'side',
+		);
+	}
+
+	/**
+	 * Adds media script to admin page.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 */
+	public static function add_image_media_script() : void {
+		wp_enqueue_media();
+		wp_enqueue_script(
+			'movie-library-image-upload-handler',
+			MOVIE_LIBRARY_PLUGIN_URL . 'admin/js/image-upload-handler.js',
+			array( 'jquery', 'wp-i18n' ),
+			MOVIE_LIBRARY_VERSION,
+			true
+		);
+	}
+
+	/**
+	 * Renders images meta box.
+	 *
+	 * @param \WP_Post $post Post object.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 */
+	public static function render_image_meta_box( \WP_Post $post ) : void {
+		// Get metadata for images.
+		$images = get_post_meta( $post->ID, 'rt-media-meta-images', true );
+
+		// If images is not an array, make it an array.
+		$images = is_array( $images ) ? $images : array();
+
+		// Remove images which are not present in media library.
+		$images = self::remove_unlinked_images( $images );
+
+		// Build image tags list.
+		$image_list = self::build_image_tags_list( $images );
+
+		$input_value = implode( ',', $images );
+
+		// Add the HTML in Meta Box.
+		?>
+		<div id='movie-library-image-upload-handler'>
+			<div id='image-preview-container'>
+				<?php echo $image_list; ?>
+			</div>
+			<button id='add-images-custom-btn' class='button button-primary widefat'>
+				<?php esc_html_e( 'Add Images', 'movie-library' ); ?>
+			</button>
+			<input
+				type='hidden'
+				name='rt-upload-images'
+				id='rt-upload-images'
+				value='<?php echo esc_attr( $input_value ); ?>'
+			/>
+		<?php
+	}
+
+	/**
+	 * Saves images meta box data.
+	 *
+	 * @param int $post_id Post ID.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 */
+	public static function save_post_images( int $post_id ) : void {
+		// Check whether request type is post and if rt-upload-images is set.
+		if ( ! isset($_POST) || ! isset( $_POST['rt-upload-images'] ) ) {
+			return;
+		}
+
+		// Sanitize and explode the data.
+		$images = filter_input(
+			INPUT_POST,
+			'rt-upload-images',
+			FILTER_SANITIZE_STRING,
+			FILTER_REQUIRE_SCALAR
+		);
+		$images = explode( ',', $images );
+
+		// sanitize the values by remove not integer values.
+		$image_ids = [];
+		foreach ( $images as $image ) {
+			if( (int) $image ) {
+				$image_ids[] = (int)$image;
+			}
+		}
+
+		// Update the post meta.
+		update_post_meta( $post_id, 'rt-media-meta-images', $image_ids );
+	}
+
+	/**
+	 * Removes images which are not present in media library.
+	 *
+	 * @param array $images Array of image ids.
+	 *
+	 * @return array
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 */
+	public static function remove_unlinked_images( array $images ) : array {
+		return array_filter(
+			$images,
+			function( $image ) {
+				return wp_get_attachment_url( $image ) !== false;
+			}
+		);
+	}
+
+	/**
+	 * Builds image tags list.
+	 *
+	 * @param array $images Array of image ids.
+	 *
+	 * @return string
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 */
+	public static function build_image_tags_list( array $images ) : string {
+		$image_list = '';
+		foreach ( $images as $image ) {
+			// Build image tag.
+			$image_item = sprintf(
+				'<img src="%s" alt="%s" class="widefat" />',
+				esc_attr( wp_get_attachment_url( $image ) ),
+				esc_attr__( get_the_title( $image ) )
+			);
+
+			// Build remove button.
+			$remove_btn = sprintf(
+				'<button class="button rt-remove-image-btn widefat" data-image-id="%d"> Remove </button>',
+				$image
+			);
+
+			// Build image item.
+			$image_list .= sprintf(
+				'<div class="image-item widefat" data-image-id="%d">%s%s</div>',
+				$image,
+				$image_item,
+				$remove_btn
+			);
+		}
+
+		return $image_list;
+	}
+}
