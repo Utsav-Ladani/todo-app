081c07efcffa5e9af7c0fba50303f938d51b2924
Utsav-Ladani <201901076@daiict.ac.in>
2023-03-03 15:34:47 +0530

    Fix: remove unnecessary lines from the plugin header.

--- plugins/movie-library/movie-library.php
+++ plugins/movie-library/movie-library.php
@@ -1,25 +1,18 @@
 <?php
 /**
- * Movie Library
- *
- * @package           MovieLibrary
- * @author            rtCamp
- * @copyright         2023 by rtCamp
- * @license           GPL-2.0-or-later
- *
- * @wordpress-plugin
  * Plugin Name:       Movie Library
- * Plugin URI:        https://example.com/movie-library
  * Description:       Plugin to manage movie library
  * Version:           1.0.0
- * Requires at least: 6.1
- * Requires PHP:      8.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.3
  * Author:            rtCamp
  * Author URI:        https://github.com/rtCamp/
  * Text Domain:       movie-library
+ * Domain Path:       /languages
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Update URI:        https://example.com/my-plugin/
+ *
+ * @package Movie_Library
  */
 
 namespace Movie_Library;
@@ -27,8 +20,6 @@ namespace Movie_Library;
 require_once __DIR__ . '/class-movie-library.php';
 
 \Movie_Library\Movie_Library::init();
-
-use Movie_Library\Movie_Library;
 
 // register activation hook.
 register_activation_hook( __FILE__, array( Movie_Library::class, 'activate' ) );
