3818f3d3557f3521cf5f33e7cd9f07ed5f3c77f7
Utsav-Ladani <201901076@daiict.ac.in>
2023-03-26 20:14:44 +0530

    Add index.php page with The LOOP

--- themes/smartdroid/index.php
+++ themes/smartdroid/index.php
@@ -9,4 +9,18 @@
 
 get_header();
 
+printf( '<main id="home-page" class="home-page">' );
+
+// The Loop
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'templates/post-card' );
+	}
+} else {
+	// no posts found
+}
+
+printf( '</main>' );
+
 get_footer();
